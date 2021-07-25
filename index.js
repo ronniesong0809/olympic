28bfd090bb97e430694ade8eaad9552ef706edc4
Ronnie Song <ronniesong0809@gmail.com>
2021-07-25 14:54:13 -0700

    save fetched data to file

--- index.js
+++ index.js
@@ -1,24 +1,41 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
+const fs = require("fs");
 
 const fetchData = () => {
   const url =
     "https://olympics.com/tokyo-2020/olympic-games/en/results/all-sports/medal-standings.htm";
+  const headers = {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+  };
 
-  axios.get(url).then(response => {
+  return axios.get(url, headers).then(response => {
     const $ = cheerio.load(response.data);
+    const data = [];
 
     $("#medal-standing > table > tbody > tr").each((_, tr) => {
       const tds = $(tr).find("td");
-      const country = tds.eq(1).text().trim();
-      const gold = Number(tds.eq(2).text());
-      const silver = Number(tds.eq(3).text());
-      const bronze = Number(tds.eq(4).text());
-      const total_medals = Number(tds.eq(5).text());
-      const total_rank = Number(tds.eq(6).text());
-      console.log(country, gold, silver, bronze, total_medals, total_rank);
+      const last_updated = Date.now();
+
+      data.push({
+        country: tds.eq(1).text().trim(),
+        gold: Number(tds.eq(2).text()),
+        silver: Number(tds.eq(3).text()),
+        bronze: Number(tds.eq(4).text()),
+        total_medals: Number(tds.eq(5).text()),
+        total_rank: Number(tds.eq(6).text()),
+        last_updated
+      });
     });
+    return data;
   });
 };
 
-fetchData();
+const saveToFile = data => {
+  fs.writeFileSync("./data/data.json", JSON.stringify(data, null, 2));
+};
+
+fetchData().then(data => {
+  saveToFile(data);
+});
